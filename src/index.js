c4dfa9e8d039daa4bb9930b86ccea4c951e4d3e7
Steven McVey <smcvey1210@gmail.com>
2022-08-23 22:32:20 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const inputValue = e.target.children[1].value;
+
+    fetch(`http://localhost:3000/movies/${inputValue}`)
+    .then(res => res.json())
+    .then(data => {
+      const title = document.getElementById('movieDetails').children[0];
+      const summary = document.getElementById('movieDetails').children[1];
+
+      title.innerText = data.title;
+      summary.innerText = data.summary;
+    })
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
